780e90d3dcc5bc66ef3a902082f811f763228b46
abhijithp327 <87142203+abhijithp327@users.noreply.github.com>
2025-03-12 13:14:07 +0530

    created user model

--- backend/src/models/auth/userModel.ts
+++ backend/src/models/auth/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { BlockList } from "net";
 
 const UserSchema = new mongoose.Schema({
 
@@ -17,10 +18,35 @@ const UserSchema = new mongoose.Schema({
     usr_password: {
         type: String,
         required: true
+    },
+
+    usr_photo: {
+        type: String,
+        default: "https://avatars.githubusercontent.com/u/19819005?v=4"
+    },
+
+    usr_bio: {
+        type: String,
+        default: "I am a new user."
+    },
+
+    usr_role: {
+        type: String,
+        enum: ["user", "admin", "creator"],
+        default: "user"
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
     }
 
-});
-
+},
+    {
+        timestamps: true,
+        minimize: true
+    }
+);
 
 
 export default mongoose.model('User', UserSchema);
